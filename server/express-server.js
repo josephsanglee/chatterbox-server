cb253a95c645ca2b44b88b17b68c1c46ba6bda79
Joseph Lee <josephsanglee@gmail.com>
2016-10-04 20:58:55 -0700

    Read files faster

--- server/express-server.js
+++ server/express-server.js
@@ -8,25 +8,30 @@ var app = express();
 app.use(express.static( __dirname + '/../client'));
 
 /********************************* FILE SYSTEM CODE *******************************/
-var initial = {
-  results: [{username: 'chatterbot', text: 'Welcome!', roomname: 'lobby', date: new Date()}]
-};
+// var initial = {
+//   results: [{username: 'chatterbot', text: 'Welcome!', roomname: 'lobby', date: new Date()}]
+// };
 
 var messages;
 //write the initial file if it doesn't exist yet
-fs.open('./data.json', 'r', function (err, data) {
-  if (err) {
-    fs.writeFile('./data.json', JSON.stringify(initial), 'utf8', function(err) {
-      if (err) { return console.log(err); }
-      console.log('success');
-    });
-  }
+// fs.open('./data.json', 'r', function (err, data) {
+//   if (err) {
+//     fs.writeFile('./data.json', JSON.stringify(initial), 'utf8', function(err) {
+//       if (err) { return console.log(err); }
+//       console.log('success');
+//     });
+//   }
 
-  fs.readFile('./data.json', 'utf8', function(err, data) {
-    if (err) { return console.log(err); }
+//   fs.readFile('./data.json', 'utf8', function(err, data) {
+//     if (err) { return console.log(err); }
     
-    messages = JSON.parse(data);
-  });
+//     messages = JSON.parse(data);
+//   });
+// });
+fs.readFile('./data.json', 'utf8', function(err, data) {
+  if (err) { return console.log(err); }
+  
+  messages = JSON.parse(data);
 });
 
 /***********************************************************************************/
